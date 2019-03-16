d90818acaa03ae025bc0feef6ba5717460d9e6ff
mkscottage <mkscottage@gmail.com>
2019-03-16 23:38:25 +0530

    deleted comments

--- unital/main_app/college_view.py
+++ unital/main_app/college_view.py
@@ -8,12 +8,6 @@ from django.shortcuts import get_object_or_404
 class CollegeHomepageView(TemplateView):
     template_name = 'college/college-homepage.html'
     def get_context_data(self, **kwargs):
-        # # college = get_object_or_404(College, clg_u_name=kwargs['clg_u_name'])
-        # college = 'mks'
-        # print(kwargs)
         context = super(CollegeHomepageView, self).get_context_data(**kwargs)
-        # # context['college_name'] = Notice.objects.order_by('-pub_date', '-id')[0:10]
-        # # context['college_list'] = College.objects.all()
-        context['college'] = College.objects.get(
-            clg_u_name=kwargs['clg_u_name'])
+        context['college'] = College.objects.get(clg_u_name=kwargs['clg_u_name'])
         return context
